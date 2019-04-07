ca35178cf07c84cbf46c64208622f6fa43a994fc
Sven Werner <privat@svenwerner.de>
2019-04-07 13:08:10 +0200

    Simplied MQTT publish and included variables

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var payload = '';
 var client  = mqtt.connect('mqtt://' + process.env.MQTT_SERVER);
 
 client.on('connect', function () {
-      //client.publish('pulsecounter/state', 'online');
+      client.publish(process.env.STATE_TOPIC, 'online');
 });
 
 net.createServer(function(sock) {
@@ -17,29 +17,13 @@ net.createServer(function(sock) {
     	payload += data;
     });
     
-    sock.on('close', function(data) {
-    	// parse JSON
-    	try {
-			// HACK: wifficounter send EOF as last character, remove it
-    		var json = JSON.parse(payload.substring(0, payload.length-1));
-    	} catch (e) {
-    		console.log('Received invalid json (' + e + ') from ' + sock.remoteAddress + ':\n' + payload);
-    		return;
-    	}
-    	
-    	// Build state object based on modultyp and system info
-    	var modultyp = { "modultyp": json.modultyp };
-    	var sysinfo = json.Systeminfo;
-    	var state = Object.assign(modultyp, sysinfo);
-    	
-        // MQTT publish
-        client.publish('pulsecounter/state', JSON.stringify(state));
-        for (var i = 0; i < json.vars.length; i++) {
-        	client.publish('pulsecounter/tele', JSON.stringify(json.vars[i]));
-    	}
+    sock.on('close', function(data) {    	
+		// MQTT publish
+		// HACK: wifficounter send EOF as last character, remove it
+        client.publish(process.env.TELE_TOPIC, payload.substring(0, payload.length-1));
     	console.log('Data received from ' + sock.remoteAddress + ':\n' + payload);
         console.log('Client disconnected: ' + sock.remoteAddress + ' ' + sock.remotePort + '\n');
         payload = '';
-    })
+    });
     
-}).listen(8181);
+}).listen(process.env.PORT);
