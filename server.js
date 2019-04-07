dd604751afbaaceacb91dd88b9fca6e88d3e154d
Sven Werner <privat@svenwerner.de>
2019-04-07 12:57:50 +0200

    semicolons added

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-var net = require('net')
-var mqtt = require('mqtt')
+var net = require('net');
+var mqtt = require('mqtt');
 
-var payload = ''
+var payload = '';
 
-var client  = mqtt.connect('mqtt://' + process.env.MQTT_SERVER)
+var client  = mqtt.connect('mqtt://' + process.env.MQTT_SERVER);
 
 client.on('connect', function () {
-      //client.publish('pulsecounter/state', 'online')
-})
+      //client.publish('pulsecounter/state', 'online');
+});
 
 net.createServer(function(sock) {
     
-    console.log('Client connected: ' + sock.remoteAddress + ':' + sock.remotePort + '\n')
+    console.log('Client connected: ' + sock.remoteAddress + ':' + sock.remotePort + '\n');
 
     sock.on('data', function(data) {
-    	payload += data
-    })
+    	payload += data;
+    });
     
     sock.on('close', function(data) {
     	// parse JSON
@@ -23,23 +23,23 @@ net.createServer(function(sock) {
 			// HACK: wifficounter send EOF as last character, remove it
     		var json = JSON.parse(payload.substring(0, payload.length-1));
     	} catch (e) {
-    		console.log('Received invalid json (' + e + ') from ' + sock.remoteAddress + ':\n' + payload)
-    		return
+    		console.log('Received invalid json (' + e + ') from ' + sock.remoteAddress + ':\n' + payload);
+    		return;
     	}
     	
     	// Build state object based on modultyp and system info
-    	var modultyp = { "modultyp": json.modultyp }
-    	var sysinfo = json.Systeminfo
-    	var state = Object.assign(modultyp, sysinfo)
+    	var modultyp = { "modultyp": json.modultyp };
+    	var sysinfo = json.Systeminfo;
+    	var state = Object.assign(modultyp, sysinfo);
     	
         // MQTT publish
-        client.publish('pulsecounter/state', JSON.stringify(state))
+        client.publish('pulsecounter/state', JSON.stringify(state));
         for (var i = 0; i < json.vars.length; i++) {
-        	client.publish('pulsecounter/tele', JSON.stringify(json.vars[i]))
+        	client.publish('pulsecounter/tele', JSON.stringify(json.vars[i]));
     	}
-    	console.log('Data received from ' + sock.remoteAddress + ':\n' + payload) 
-        console.log('Client disconnected: ' + sock.remoteAddress + ' ' + sock.remotePort + '\n')
-        payload = ''
+    	console.log('Data received from ' + sock.remoteAddress + ':\n' + payload);
+        console.log('Client disconnected: ' + sock.remoteAddress + ' ' + sock.remotePort + '\n');
+        payload = '';
     })
     
-}).listen(8181)
+}).listen(8181);
